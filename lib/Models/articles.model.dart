f277ab9a3dca940eab40e8cb01d8c081236c414e
Juan David Pareja Soto <parejajd@outlook.com>
2021-09-17 19:23:47 -0500

    Modelos y provider creado

---
+++ lib/Models/articles.model.dart
@@ -0,0 +1,27 @@
+//Article model From newsapi.org
+class Article {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Article(
+      {required this.source,
+      required this.author,
+      required this.title,
+      required this.description,
+      required this.url,
+      required this.urlToImage,
+      required this.publishedAt,
+      required this.content});
+}
+
+class Source {
+  String id;
+  String name;
+  Source({required this.id, required this.name});
+}
